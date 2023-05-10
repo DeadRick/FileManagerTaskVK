1e5b48423de5bd1d00b35f2c89ef2eea5679118e
Victor <39325834+DeadRick@users.noreply.github.com>
2023-05-10 20:49:22 +0300

    Added sort by name, by size

--- app/src/main/java/com/example/myfilemanager/FileListActivity.java
+++ app/src/main/java/com/example/myfilemanager/FileListActivity.java
@@ -9,6 +9,10 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class FileListActivity extends AppCompatActivity {
 
@@ -30,8 +34,32 @@ public class FileListActivity extends AppCompatActivity {
             return;
         }
 
+        Arrays.sort(allFiles, new FileNameComparator());
+
         filesInfo.setVisibility(recyclerView.INVISIBLE);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new FileAdapter(getApplicationContext(), allFiles));
+    }
+
+    public class FileNameComparator implements Comparator<File> {
+        @Override
+        public int compare(File file1, File file2) {
+            return file1.getName().compareToIgnoreCase(file2.getName());
+        }
+    }
+
+    // Compare file by size
+    public class FileSizeComparator implements Comparator<File> {
+        @Override
+        public int compare(File file1, File file2) {
+            long diff = file1.length() - file2.length();
+            if (diff > 0) {
+                return 1;
+            } else if (diff < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
     }
 }
