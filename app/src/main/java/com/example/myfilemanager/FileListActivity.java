a8be88ba1507652f6e5116d47193d22a6aec52a9
Victor <39325834+DeadRick@users.noreply.github.com>
2023-05-10 22:03:08 +0300

    Added ascending and descending sort

--- app/src/main/java/com/example/myfilemanager/FileListActivity.java
+++ app/src/main/java/com/example/myfilemanager/FileListActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.Arrays;
@@ -25,6 +26,9 @@ public class FileListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private File[] allFiles;
+    private boolean isExtensionSort = false;
+    private boolean isNameSort = false;
+    private boolean isSizeSort = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,7 @@ public class FileListActivity extends AppCompatActivity {
             return;
         }
 
-        Arrays.sort(allFiles, new FileNameComparator());
+        Arrays.sort(allFiles, new FileNameComparatorAscending());
 
         filesInfo.setVisibility(recyclerView.INVISIBLE);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -61,28 +65,61 @@ public class FileListActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-
+        String sortText = "";
         if (id == R.id.sort_by_name) {
-            Arrays.sort(allFiles, new FileNameComparator());
+            if (isNameSort) {
+                isNameSort = false;
+                Arrays.sort(allFiles, new FileNameComparatorAscending());
+                sortText = "Ascending sort";
+            } else {
+                isNameSort = true;
+                Arrays.sort(allFiles, new FileNameComparatorDescending());
+                sortText = "Descending sort";
+            }
         } else if (id == R.id.sort_by_size) {
-            Arrays.sort(allFiles, new FileSizeComparator());
+            if (isSizeSort) {
+                isSizeSort = false;
+                Arrays.sort(allFiles, new FileSizeComparatorAscending());
+                sortText = "Ascending sort";
+            } else {
+                isSizeSort = true;
+                Arrays.sort(allFiles, new FileSizeComparatorDescending());
+                sortText = "Descending sort";
+
+            }
+        } else if (id == R.id.sort_by_extension) {
+            if (isExtensionSort) {
+                isExtensionSort = false;
+                Arrays.sort(allFiles, new FileExtensionComparatorAscending());
+                sortText = "Ascending sort";
+            } else {
+                isExtensionSort = true;
+                Arrays.sort(allFiles, new FileExtensionComparatorDesceding());
+                sortText = "Descending sort";
+            }
         }
-
-
+        Toast.makeText(getApplicationContext(), sortText, Toast.LENGTH_SHORT).show();
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new FileAdapter(getApplicationContext(), allFiles));
         return super.onOptionsItemSelected(item);
     }
 
-    public class FileNameComparator implements Comparator<File> {
+    public class FileNameComparatorAscending implements Comparator<File> {
         @Override
         public int compare(File file1, File file2) {
             return file1.getName().compareToIgnoreCase(file2.getName());
         }
     }
 
+    public class FileNameComparatorDescending implements Comparator<File> {
+        @Override
+        public int compare(File file1, File file2) {
+            return file2.getName().compareToIgnoreCase(file1.getName());
+        }
+    }
+
     // Compare file by size
-    public class FileSizeComparator implements Comparator<File> {
+    public class FileSizeComparatorAscending implements Comparator<File> {
         @Override
         public int compare(File file1, File file2) {
             long diff = file1.length() - file2.length();
@@ -92,6 +129,59 @@ public class FileListActivity extends AppCompatActivity {
                 return -1;
             } else {
                 return 0;
+            }
+        }
+    }
+
+    public class FileSizeComparatorDescending implements Comparator<File> {
+        @Override
+        public int compare(File file1, File file2) {
+            long diff = file1.length() - file2.length();
+            if (diff < 0) {
+                return 1;
+            } else if (diff > 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+
+    public class FileExtensionComparatorAscending implements Comparator<File> {
+        @Override
+        public int compare(File file1, File file2) {
+            String ext1 = getExtension(file1);
+            String ext2 = getExtension(file2);
+            return ext1.compareTo(ext2);
+        }
+
+        private String getExtension(File file) {
+            String name = file.getName();
+            int index = name.lastIndexOf(".");
+            if (index > 0 && index < name.length() - 1) {
+                return name.substring(index + 1).toLowerCase();
+            } else {
+                return "";
+            }
+        }
+    }
+
+    public class FileExtensionComparatorDesceding implements Comparator<File> {
+        @Override
+        public int compare(File file1, File file2) {
+            String ext1 = getExtension(file1);
+            String ext2 = getExtension(file2);
+            return ext2.compareTo(ext1);
+        }
+
+        private String getExtension(File file) {
+            String name = file.getName();
+            int index = name.lastIndexOf(".");
+            if (index > 0 && index < name.length() - 1) {
+                return name.substring(index + 1).toLowerCase();
+            } else {
+                return "";
             }
         }
     }
